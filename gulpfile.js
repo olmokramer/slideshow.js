f6b441a69d7376b1e83fe051d79c354e4f665a8c
Olmo Kramer <olmo.kramer@gmail.com>
2015-02-01 00:17:33 +0100

    gulp build system

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+gulp = require('gulp');
+coffee = require('gulp-coffee');
+uglify = require('gulp-uglify');
+rename = require('gulp-rename');
+
+gulp.task('compile', function() {
+  gulp.src('slideshow.coffee')
+    .pipe(coffee())
+    .pipe(gulp.dest('.'))
+    .pipe(uglify())
+    .pipe(rename('slideshow.min.js'))
+    .pipe(gulp.dest('.'));
+});
