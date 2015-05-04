d78f640a7c4e1eae502955394762d970421bbcb2
Olmo Kramer <olmo.kramer@gmail.com>
2015-05-04 22:04:05 +0200

    sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 gulp = require('gulp');
 coffee = require('gulp-coffee');
 uglify = require('gulp-uglify');
+sourcemaps = require('gulp-sourcemaps');
 rename = require('gulp-rename');
 
-gulp.task('watch', function() {
+gulp.task('watch', ['compile'], function() {
   gulp.watch('slideshow.coffee', ['compile']);
 });
 
 gulp.task('compile', function() {
   gulp.src('slideshow.coffee')
+    .pipe(sourcemaps.init())
     .pipe(coffee())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('.'))
     .pipe(uglify())
     .pipe(rename('slideshow.min.js'))
