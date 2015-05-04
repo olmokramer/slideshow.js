836c61141f9fca231938d28c49b2e4bdcf1dfea0
Olmo Kramer <olmo.kramer@gmail.com>
2015-05-04 23:22:30 +0200

    separate sourcemap files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+path = require('path');
 gulp = require('gulp');
 coffee = require('gulp-coffee');
 uglify = require('gulp-uglify');
 sourcemaps = require('gulp-sourcemaps');
+ignore = require('gulp-ignore');
 rename = require('gulp-rename');
 
 gulp.task('watch', ['compile'], function() {
@@ -12,10 +14,15 @@ gulp.task('compile', function() {
   gulp.src('slideshow.coffee')
     .pipe(sourcemaps.init())
     .pipe(coffee())
-    .pipe(sourcemaps.write())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('.'))
+    .pipe(ignore(function(file) {
+      return path.extname(file.path) !== '.js';
+    }))
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
     .pipe(rename('slideshow.min.js'))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('.'));
 });
 
